063d44e5a779342805219f7ad0775a689b1959e1
Angel Daniel Zárate Hernández <AngelZarate@valoran.com.mx>
2023-07-03 12:48:31 -0600

    fix: order on create subsummary

--- app/Models/Subsummary.php
+++ app/Models/Subsummary.php
@@ -15,6 +15,15 @@ class Subsummary extends Model
 
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($s) {
+            $s->order = (Subsummary::where('summary_id', $s->summary_id)->max('order')??0)+1;
+        });
+    }
+
+
     public function summary(){
         return $this->belongsTo(Summary::class);
     }
